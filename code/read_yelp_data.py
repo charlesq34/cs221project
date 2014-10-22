158939ce8986ca8ea55717bf0d0b595447844dac
QRZT <QRZT@DNab4046af.stanford.edu>
2014-10-21 22:31:07 -0700

    filters out stop words

--- code/read_yelp_data.py
+++ code/read_yelp_data.py
@@ -66,7 +66,8 @@ class YelpDataExtractor():
 # Input a text string, output a list of words from the text
 def text2words(text):
     words = nltk.word_tokenize(text)
-    return [w.lower() for w in words if w not in string.punctuation]
+    words = [w.lower() for w in words if w not in string.punctuation]
+    return [w for w in words if w not in nltk.corpus.stopwords.words('english')]
 
 if __name__ == "__main__":
     reviewFile = '../data/yelp_academic_dataset_review.json'
